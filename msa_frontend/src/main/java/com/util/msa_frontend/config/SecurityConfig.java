34a7ff2a3ebe7ea2e3acfb54488925ee08d2b702
Himmel <bbluesky7738@gmail.com>
2023-05-25 22:30:45 +0900

    jwt setting

--- msa_frontend/src/main/java/com/util/msa_frontend/config/SecurityConfig.java
+++ msa_frontend/src/main/java/com/util/msa_frontend/config/SecurityConfig.java
@@ -2,14 +2,59 @@ package com.util.msa_frontend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.util.msa_frontend.security.jwt.JwtAuthenticationFilter;
+import com.util.msa_frontend.security.jwt.JwtTokenProvider;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean // login 시 실행되는 메소드
-    public AuthenticationProvider authenticationProvider() {
-        return new LoginAuthenticationProvider();
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
+
+    // authenticationManager를 Bean 등록하기
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable() // rest api 만을 고려하여 기본 설정은 해제
+                // h2 콘솔 사용
+                .csrf().disable() // csrf 보안 토큰 disable 처리
+                // 세션 사용 안함
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                // URL 관리
+                .authorizeRequests() // 요청에 대한 사용권한 체크
+                .antMatchers("/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+
+                // JwtAuthenticationFilter를 먼저 적용
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+                        UsernamePasswordAuthenticationFilter.class);
+    }
+
 }
