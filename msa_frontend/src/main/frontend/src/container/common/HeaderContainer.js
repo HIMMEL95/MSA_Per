3e64c71fb26bd62e4fd07e775041e11029d62b3b
Himmel <bbluesky7738@gmail.com>
2023-06-04 09:07:02 +0900

    add comit

--- msa_frontend/src/main/frontend/src/container/common/HeaderContainer.js
+++ msa_frontend/src/main/frontend/src/container/common/HeaderContainer.js
@@ -1,17 +1,18 @@
-import { faHubspot } from "@fortawesome/free-brands-svg-icons";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "bootstrap/dist/css/bootstrap.css";
 import Navbar from "react-bootstrap/Navbar";
+import Logo from "../../Image/honeycomb.png";
 import "../../css/HeaderContainer.css";
 
 const HeaderContainer = () => {
   return (
     <div>
-      <Navbar bg="light" variant="light">
-        <div className="container">
+      <nav className="navbar navbar-expand-lg">
+        <div className="header-container">
           <div className="title">
-            <FontAwesomeIcon icon={faHubspot} size="lg" />
+            {/* <FontAwesomeIcon icon={faHubspot} size="lg" /> */}
+            <img className="logo" src={Logo} alt="logo" />
             <Navbar.Brand href="#home">Jobhub</Navbar.Brand>
           </div>
           <div className="search-bar">
@@ -24,21 +25,33 @@ const HeaderContainer = () => {
             />
           </div>
           <div className="utility">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <div class="nav-link" aria-current="page">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <div className="nav-link" aria-current="page">
                   로그인
                 </div>
               </li>
               <hr />
-              <li class="nav-item">
-                <div class="nav-link">회원가입</div>
+              <li className="nav-item">
+                <div className="nav-link">회원가입</div>
               </li>
             </ul>
           </div>
-          <div className="navigation"></div>
+          <div className="navigation">
+            <div className="navbar-brand">Navbar</div>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav">
+                <div className="nav-link active" aria-current="page">
+                  Home
+                </div>
+                <div className="nav-link">Features</div>
+                <div className="nav-link">Pricing</div>
+                <div className="nav-link disabled">Disabled</div>
+              </div>
+            </div>
+          </div>
         </div>
-      </Navbar>
+      </nav>
     </div>
   );
 };
